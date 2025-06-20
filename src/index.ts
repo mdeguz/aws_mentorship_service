5ac8326a6c752ae7c5b98db1455bae96d4918939
Matthew De Guzman <matthew.deguzman@jane.app>
2025-06-20 14:24:26 -0700

    add rows to aws rds

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,35 @@
 import { Hono } from "@hono/hono"
 import { todos } from "./routes/index.ts"
 
-const app = new Hono()
+import { Client } from "pg"
 
-app.route("/todos", todos)
+const client = new Client({
+  user: "<SCRUBBED>",
+  password: "<SCRUBBED>",
+  host: "todos-db-instance-1.cjw40c4scek7.us-east-2.rds.amazonaws.com",
+  port: "5432",
+  database: "todos",
+})
 
-Deno.serve(app.fetch)
+await client.connect()
+const res = await client.query("SELECT NOW()")
+
+const allRes = await client.query("SELECT * FROM todos")
+
+console.log(`allRes ${JSON.stringify(allRes)}`)
+
+await client.end()
+
+// // AWS RDS
+// import { RDSClient, ListTagsForResourceCommand } from "@aws-sdk/client-rds";
+
+// // a client can be shared by different commands.
+// const client = new RDSClient({ region: "REGION" });
+
+// // end
+
+// const app = new Hono()
+
+// app.route("/todos", todos)
+
+// Deno.serve(app.fetch)
